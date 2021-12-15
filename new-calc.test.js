75c4ae5cb1ea34bb397867e95f79a82ffe14bd09
Ronald José <rjaf1@aluno.ifal.edu.br>
2021-12-15 09:47:53 -0300

    Corrigindo alguns erros

--- new-calc.test.js
+++ new-calc.test.js
@@ -25,52 +25,52 @@ test('Soma 9 e "r" espera NaN', () => {
 })
 
 // Atividade A1.2
-test('Soma 30 e 14 espera 16', () => {
+test('Subitrair 30 e 14 espera 16', () => {
     expect(calc.sub(30, 14)).toBe(16);
 })
 
-test('Soma -90 e -34 espera -56', () => {
+test('Subitrair -90 e -34 espera -56', () => {
     expect(calc.sub(-90, -34)).toBe(-56);
 })
 
-test('Soma 56 e -18 espera 74', () => {
+test('Subitrair 56 e -18 espera 74', () => {
     expect(calc.sub(56, -18)).toBe(74);
 })
 
-test('Soma 82 e 0 espera 82', () => {
+test('Subitrair 82 e 0 espera 82', () => {
     expect(calc.sub(82, 0)).toBe(82);
 })
 
 // Atividade A1.3
-test('Soma 17 e 13 espera 221', () => {
+test('Multiplicar 17 e 13 espera 221', () => {
     expect(calc.mult(17, 13)).toBe(221);
 })
 
-test('Soma -11 e -6 espera 66', () => {
+test('Multiplicar -11 e -6 espera 66', () => {
     expect(calc.mult(-11, -6)).toBe(66);
 })
 
-test('Soma 56 e -18 espera -1008', () => {
+test('Multiplicar 56 e -18 espera -1008', () => {
     expect(calc.mult(56, -18)).toBe(-1008);
 })
 
-test('Soma 82 e 0 espera 0', () => {
+test('Multiplicar 82 e 0 espera 0', () => {
     expect(calc.mult(82, 0)).toBe(0);
 })
 
 // Atividade A2.1
-test('Soma 30 e 15 espera 2', () => {
-    expect(calc.sub(30, 15)).toBe(2);
+test('Dividir 30 e 15 espera 2', () => {
+    expect(calc.div(30, 15)).toBe(2);
 })
 
-test('Soma -90 e -30 espera 3', () => {
-    expect(calc.sub(-90, -30)).toBe(3);
+test('Dividir -90 e -30 espera 3', () => {
+    expect(calc.div(-90, -30)).toBe(3);
 })
 
-test('Soma 77 e -7 espera -11', () => {
-    expect(calc.soma(77, -7)).toBe(-11);
+test('Dividir 77 e -7 espera -11', () => {
+    expect(calc.div(77, -7)).toBe(-11);
 })
 
-test('Soma 7 e 0 espera 0', () => {
-    expect(calc.soma(7, 0)).toBe(0);
+test('Dividir 7 e 0 espera 0', () => {
+    expect(calc.div(7, 0)).toBe(0);
 })
